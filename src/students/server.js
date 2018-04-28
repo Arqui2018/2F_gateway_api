b7d1a4e538411f61c7ca1b1d5479adbcfbbfaf00
Jhon Torres <jhetorresto@unal.edu.co>
2018-04-28 10:47:37 -0500

    fix errors

--- src/students/server.js
+++
@@ -1,3 +0,0 @@
-export const url = process.env.STUDENTS_URL || '192.168.99.101';
-export const port = process.env.STUDENTS_PORT || '4000';
-export const entryPoint = process.env.STUDENTS_ENTRY || 'students';
