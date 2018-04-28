e3b579c4a48f929ba3a721553f663ff215ee5b8a
cccristanchoc <cccristanchoc@unal.edu.co>
2018-04-27 20:41:14 -0500

    Se modifican url por defecto del los Microservicios

--- src/matches/server.js
+++ src/matches/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.MATCHES_URL || 'matches-ms';
+export const url = process.env.MATCHES_URL || 'matches_ms';
 export const port = process.env.MATCHES_PORT || '4003';
 export const entryPoint = process.env.MATCHES_ENTRY || 'matches';
--- src/teams/server.js
+++ src/teams/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.TEAMS_URL || 'matches-ms';
+export const url = process.env.TEAMS_URL || 'matches_ms';
 export const port = process.env.TEAMS_PORT || '4003';
 export const entryPoint = process.env.TEAMS_ENTRY || 'teams';
