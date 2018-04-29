0a98e1e6f8cae149abc7587791c5808ab60f7af3
cccristanchoc <cccristanchoc@unal.edu.co>
2018-04-29 14:52:34 -0500

    push - instead _ in server.js of wallets

--- src/wallets/server.js
+++ src/wallets/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.WALLETS_URL || 'wallets_ms';
+export const url = process.env.WALLETS_URL || 'wallets-ms';
 export const port = process.env.WALLETS_PORT || '4040';
 export const entryPoint = process.env.WALLETS_ENTRY || 'wallets';
